15d79569320e28c1fe53bd72778212212509f0a9
“Avek.io <tech@avek.io>
2022-10-06 13:54:00 +0300

    Clean project

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,7 @@
 <?php
+declare(strict_types=1);
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+use Illuminate\Routing\Router;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+/** @var Router $router */
+$router->fallback(fn () => 'test');
